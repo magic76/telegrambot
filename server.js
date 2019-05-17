5fb78c1fb08d79c35a1b14c4be53679839a92453
liyanghsiao <liyanghsiao@boyu66.net>
2019-05-17 10:07:45 +0800

    up

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '843900086:AAHXB7we1Kqh32iMjkYQXuEk_DCEXKb711M';
 
 // Create a bot that uses 'polling' to fetch new updates
-let bot = new TelegramBot(token);
+let bot = new TelegramBot(token, {webHook:{port: process.env.PORT || 443}});
 const url = process.env.APP_URL || 'https://telegrambot77.herokuapp.com:443';
-const externalUrl = process.env.CUSTOM_ENV_VARIABLE || 'https://telegrambot77.herokuapp.com',
+const externalUrl = process.env.CUSTOM_ENV_VARIABLE || 'https://telegrambot77.herokuapp.com';
 // This informs the Telegram servers of the new webhook.
 // Note: we do not need to pass in the cert, as it already provided
 // bot.setWebHook(`${externalUrl}:443/bot${token}`);
-// bot.setWebHook(`https://telegrambot77.herokuapp.com:443/bot${token}`);
+bot.setWebHook(`${externalUrl}/bot${token}`);
 
-// We are receiving updates at the route below!
-app.post(`/bot${TOKEN}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+// // We are receiving updates at the route below!
+// app.post(`/bot${TOKEN}`, (req, res) => {
+//   bot.processUpdate(req.body);
+//   res.sendStatus(200);
+// });
 
 
-// Start Express Server
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Express server is listening on ${port}`);
-});
+// // Start Express Server
+// app.listen(process.env.PORT || 8080, () => {
+//   console.log(`Express server is listening on ${port}`);
+// });
 
 
 
