c23f9b993ee19dca229eecf1521567309ce234a7
liyanghsiao <liyanghsiao@boyu66.net>
2019-05-17 10:10:02 +0800

    update

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
+  console.log(JSON.stringify(msg));
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'Received your message'+chatId);
 });
